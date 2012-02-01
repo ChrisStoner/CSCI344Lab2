e9e84e58181959d5cdb569554d754f132a9add75
Chris Stoner <cstoner@unca.edu>
2012-02-01 11:17:12 -0500

    In class changes

--- scripts/src/script.js
+++ scripts/src/script.js
@@ -1,21 +1,19 @@
-/**
- * This is the entry point for our JavaScript program
- */
 function main() {
-    //your code goes here
-    alert("hello world!");
+	var s = new Spotter("twitter.search",
+			    {q:"bieber", period:120},
+			    {buffer:true, bufferTimeout:750}
+			   );	
+	s.register(function(tweet) {
+		var profile_image = "<img src='"+tweet.profile_image_url+"' />";
+		$("#tweets").append("<p>" + profile_image + tweet.text + "</p>");
+	});
+	s.start();
 
-
-    //your tasks
-
-    //1. Create a spotter and get it to insert tweets into the DOM
-    //2. Add profile images (tweet.profile_image_url)
+	
     //3. Make the tweets occur so the most recent are at the top
     //4. Make the tweets slide down
     //5. Alternate the colors or the background of the tweets
     //6. Show a maximum of 10 tweets at a time (remove old tweets from the dom)
-
-
 }
 
 main();
